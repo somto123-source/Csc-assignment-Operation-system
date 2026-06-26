45510e38474547723f2b3471f74bfa6eb605d842
somto123-source <nnamasomto5@gmail.com>
2026-06-26 19:25:00 +0100

    With mutex.c

---
+++ Practical 1/With mutex.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define THREADS 5
+#define COUNT 100000
+
+int counter = 0;
+pthread_mutex_t mutex;
+
+void *increment(void *arg)
+{
+    for (int i = 0; i < COUNT; i++)
+    {
+        pthread_mutex_lock(&mutex);
+        counter++;
+        pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
+
+int main()
+{
+    pthread_t threads[THREADS];
+
+    pthread_mutex_init(&mutex, NULL);
+
+    for (int i = 0; i < THREADS; i++)
+        pthread_create(&threads[i], NULL, increment, NULL);
+
+    for (int i = 0; i < THREADS; i++)
+        pthread_join(threads[i], NULL);
+
+    printf("Final Counter = %d\n", counter);
+
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
+}
