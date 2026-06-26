4d852494b3678b390ec9b6097107757ac3edc470
somto123-source <nnamasomto5@gmail.com>
2026-06-26 20:15:31 +0100

    Update README formatting for practical sessions

--- README.C
+++ README.C
@@ -2,9 +2,9 @@
 
 This repository contains my practical implementations for **CSC 308 (Operating Systems)**. The programs demonstrate key operating system concepts such as thread synchronization, mutual exclusion, semaphores, the Producer-Consumer problem, and Inter-Process Communication (IPC) using shared memory.
 
-## Practical Sessions
+Practical Sessions
 
-### 1. Mutex Lock Demonstration
+## Mutex Lock Demonstration
 
 **Objective**
 - Demonstrate mutual exclusion using POSIX mutex locks.
